586f746207d32a05fe33893a099388cfc98dd5df
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-28 22:26:06 +0300

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      safa
+    </div>
+  );
+}
+
+export default App;
